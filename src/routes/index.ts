ae326858c1b63c5149018f037119da14197114ec
ali.jafari <alijch@gmail.com>
2025-09-24 17:44:55 +0330

    feat: add notFoundHandler middleware to handle 404 errors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import weatherRoutes from "./weather";
 import authRoutes from "./auth";
+import { notFoundHandler } from "../middleware/notFoundHandler";
 
 const router = Router();
 
 router.use("/weather", weatherRoutes);
 router.use("/auth", authRoutes);
+router.use(notFoundHandler);
 
 export default router;
